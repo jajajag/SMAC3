a14b63e4e7e01478de0499d19605a9ca4ab36dbb
Jag Zhang <zzzsgsb@gmail.com>
2018-12-03 16:10:07 +0800

    Update the full test file for pssmac.

--- test_ps.py
+++ test_ps.py
@@ -1,71 +1,54 @@
 from fabric import Connection
 from absl import app
 
+# 配置文件放在这里
 nodes = [{
-    'host': '54.80.187.152',
-    'key': './id_rsa_zhanghanping',
-    'user': 'zhanghanping',
+    'ssh_host': '54.80.187.152',
+    'ssh_key': 'smac/pssmac/utils/id_rsa_zhanghanping',
+    'ssh_user': 'zhanghanping',
     'ssh_port': 22,
+    'smac_dir': '$HOME/smac/',
+    'data_dir': '$HOME/smac/data/libsvm_real-sim',
+    'temp_dir': '$HOME/smac/tmp/',
+    'output_dir': '$HOME/smac/output/',
+    'node_host': '172.31.29.227',
     'node_port': [6666, 23333],
     'job': ['server', 'worker'],
     'id': [0, 0],
-    'cutoff': [0, 60]
-    'smac': '$HOME/smac',
-    'data_dir': '$HOME/data/libsvm_real-sim',
-    'temp_dir': '$HOME/tmp/',
+    'cutoff': [0, 60],
 }, {
-    'host': '52.80.187.152',
-    'key': './id_rsa_zhanghanping',
-    'user': 'zhanghanping',
+    'ssh_host': '54.223.203.214',
+    'ssh_key': 'smac/pssmac/utils/id_rsa_zhanghanping',
+    'ssh_user': 'zhanghanping',
     'ssh_port': 22,
+    'smac_dir': '$HOME/smac/',
+    'data_dir': '$HOME/smac/data/libsvm_real-sim',
+    'temp_dir': '$HOME/smac/tmp/',
+    'output_dir': '$HOME/smac/output/',
+    'node_host': '172.31.21.27',
     'node_port': [6666, 23333],
     'job': ['scheduler', 'worker'],
     'id': [0, 1],
-    'cutoff': [0, 60]
-    'smac': '$HOME/smac',
-    'data_dir': '$HOME/data/libsvm_real-sim',
-    'temp_dir': '$HOME/tmp/',
+    'cutoff': [0, 60],
 }, {
-    'host': '52.80.220.75',
-    'key': './id_rsa_zhanghanping',
-    'user': 'zhanghanping',
+    'ssh_host': '52.80.220.75',
+    'ssh_key': 'smac/pssmac/utils/id_rsa_zhanghanping',
+    'ssh_user': 'zhanghanping',
     'ssh_port': 22,
+    'smac_dir': '$HOME/smac/',
+    'data_dir': '$HOME/smac/data/libsvm_real-sim',
+    'temp_dir': '$HOME/smac/tmp/',
+    'output_dir': '$HOME/smac/output/',
+    'node_host': '172.31.28.149',
     'node_port': [23333],
     'job': ['worker'],
     'id': [2],
-    'cutoff': [60]
-    'smac': '$HOME/smac',
-    'data_dir': '$HOME/data/libsvm_real-sim',
-    'temp_dir': '$HOME/tmp/',
+    'cutoff': [60],
 }]
-
-"""
-nohup
-python3
-test_ps.py
---ps=./smac/pssmac/ps/ps_smac
---data_dir=./data/libsvm_real-sim
---node=worker
---temp_dir=./tmp/
---num_servers=1
---num_workers=2
---scheduler_host=172.31.21.27
---scheduler_port=6666
---node_host=172.31.29.227
---node_port=23333
---id=0
---cutoff=60
-1 > output/worker0_output.txt
-&
-
-"""
 
 
 def main(argv):
-    # 所有flags大集合
-    flags_list = ['execute', 'ps', 'data_dir', 'node', 'temp_dir',
-                  'num_servers', 'num_workers', 'scheduler_host',
-                  'scheduler_port', 'node_host', 'node_port', 'id', 'cutoff']
+    # 统计server和worker数量
     num_servers, num_workers = 0, 0
     for node in nodes:
         for i in range(len(node['job'])):
@@ -74,28 +57,42 @@ def main(argv):
             elif node['job'][i] == 'worker':
                 num_workers += 1
             elif node['job'][i] == 'scheduler':
-                scheduler_host = node['']
+                scheduler_host = node['node_host']
+                scheduler_port = node['node_port'][i]
             else:
-                raise KeyError("Please specify a valid job.")
-
+                raise ValueError("Please specify a valid job.")
 
     for node in nodes:
         # 建立fabric的连接
-        connection = Connection(node['host'],
-                                connect_kwargs={'key_filename': node['key']},
-                                user=node['user'], port=node['ssh_port'])
-        # cd到smac的目录下
-        connection.run('cd ' + node['smac'])
-        #
+        connection = Connection(node['ssh_host'],
+                                connect_kwargs={
+                                    'key_filename': node['ssh_key']},
+                                user=node['ssh_user'],
+                                port=node['ssh_port'])
+        # 所有flags大集合
         flags_dict = {}
-        execute = node['smac'] + '/pssmac/utils/run_facade.py'
-        flags_dict['ps'] = node['smac'] + '/pssmac/ps/ps_smac'
+        execute = node['smac_dir'] + 'pssmac/utils/run_facade.py'
+        flags_dict['ps'] = node['smac_dir'] + 'pssmac/ps/ps_smac'
         flags_dict['data_dir'] = node['data_dir']
         flags_dict['temp_dir'] = node['temp_dir']
-        flags_dict['num_server'] =
+        flags_dict['num_servers'] = num_servers
+        flags_dict['num_workers'] = num_workers
+        flags_dict['scheduler_host'] = scheduler_host
+        flags_dict['scheduler_port'] = scheduler_port
+        flags_dict['node_host'] = node['node_host']
 
-        flags = ' '.join([('--' + key + '=' + str(val)) for (key, val) in
-                          flags_dict.items()])
+        for i in range(len(node['job'])):
+            # 装填针对每个node的flag
+            flags_dict['node'] = node['job'][i]
+            flags_dict['node_port'] = node['node_port'][i]
+            flags_dict['id'] = node['id'][i]
+            flags_dict['cutoff'] = node['cutoff'][i]
+            flags = ' '.join([('--' + key + '=' + str(val)) for (key, val) in
+                              flags_dict.items()])
+            flags = "nohup python3 " + execute + " " + flags + ' > ' + node[
+                'output_dir'] + node['job'][i] + str(node['id'][i]) + '.txt &'
+            # 运行
+            connection.run(flags)
 
 
 if __name__ == '__main__':
